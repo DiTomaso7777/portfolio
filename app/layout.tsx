de1dcea8e4410e75a668c6fac463c60a9d1c5e9d
DallasMaverick7777 <marius.radulescu@tomasini.de>
2025-01-03 15:01:24 +0100

    Security

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'" />
+      </head>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.ibb.co',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+        ],
+      },
+    ]
+  },
+};
+
+export default nextConfig;
