2539548f2eed6294fd0712d4bdc45dc512321c35
DallasMaverick7777 <marius.radulescu@tomasini.de>
2025-01-03 15:06:20 +0100

    Security

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains'
           },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; frame-src 'self' https://outlook.office365.com;"
+          },
         ],
       },
     ]
